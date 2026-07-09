bdd911b1ced1a67886a0807e4e3be5832462d9d7
Chong Rui Xin <rxchong1123@gmail.com>
2026-07-09 22:20:29 +0800

    Concatenate Non-Zero Digits and Multiply By Sum II

--- code1.js
+++ code1.js
@@ -20,3 +20,40 @@ var romanToInt = function (s) {
 
   return res + roman[s[s.length - 1]];
 };
+
+const nMax = 1e5,
+  mod = 1e9 + 7,
+  bmod = BigInt(mod),
+  powMod = new BigInt64Array(nMax + 1).fill(1n, 0, 1);
+for (let i = 1; i <= nMax; i++) powMod[i] = (powMod[i - 1] * 10n) % bmod;
+
+var sumAndMultiply = function (s, queries) {
+  const n = s.length,
+    m = queries.length,
+    sumPfs = new Uint32Array(n + 1),
+    concatPfs = new Uint32Array(n + 1),
+    countPfs = new Uint32Array(n + 1);
+  // O(n): Build prefix sum tables
+  for (let i = 0; i < n; i++) {
+    const x = ~~s[i];
+    sumPfs[i + 1] = sumPfs[i] + x;
+    if (x) {
+      concatPfs[i + 1] = (concatPfs[i] * 10 + x) % mod;
+      countPfs[i + 1] = countPfs[i] + 1;
+    } else {
+      concatPfs[i + 1] = concatPfs[i];
+      countPfs[i + 1] = countPfs[i];
+    }
+  }
+  // O(m): Calculate query answers using prefix sum tables
+  for (let q = 0; q < m; q++) {
+    const [ql, qr] = queries[q],
+      count = countPfs[qr + 1] - countPfs[ql],
+      sum = sumPfs[qr + 1] - sumPfs[ql],
+      concatR = concatPfs[qr + 1],
+      concatL = Number((BigInt(concatPfs[ql]) * powMod[count]) % bmod),
+      concat = (concatR - concatL + mod) % mod;
+    queries[q] = (concat * sum) % mod;
+  }
+  return queries;
+};
