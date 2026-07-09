a4334dcca62957c6dd32af3a7e79e10638f4f028
Chong Rui Xin <rxchong1123@gmail.com>
2026-07-09 22:22:31 +0800

    3Sum Closest

--- code1.js
+++ code1.js
@@ -76,3 +76,36 @@ var sumAndMultiply = function (n) {
 
   return x * sum;
 };
+
+console.log("hello");
+
+var threeSumClosest = function (nums, target) {
+  nums.sort((a, b) => a - b);
+  let closest = nums[0] + nums[1] + nums[2];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    // skip duplicate i values (optional optimization)
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (Math.abs(sum - target) < Math.abs(closest - target)) {
+        closest = sum;
+      }
+
+      if (sum === target) {
+        return sum; // can't get closer than exact match
+      } else if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return closest;
+};
