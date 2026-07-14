bbc0405082080dd34269c04f5e8098544af98d09
Chong Rui Xin <rxchong1123@gmail.com>
2026-07-14 20:39:01 +0800

    generate parentheses

--- code1.js
+++ code1.js
@@ -109,3 +109,25 @@ var threeSumClosest = function (nums, target) {
 
   return closest;
 };
+
+var generateParenthesis = function (n) {
+  const result = [];
+
+  function backtrack(current, open, close) {
+    if (current.length === 2 * n) {
+      result.push(current);
+      return;
+    }
+
+    if (open < n) {
+      backtrack(current + "(", open + 1, close);
+    }
+
+    if (close < open) {
+      backtrack(current + ")", open, close + 1);
+    }
+  }
+
+  backtrack("", 0, 0);
+  return result;
+};
