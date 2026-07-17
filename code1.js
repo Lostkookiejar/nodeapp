5e0bf9790fd271516b05901fff3e314dd1695216
Chong Rui Xin <rxchong1123@gmail.com>
2026-07-17 10:56:18 +0800

    Remove Element

--- code1.js
+++ code1.js
@@ -152,3 +152,14 @@ var isValid = function (s) {
 
   return stack.length === 0;
 };
+
+var removeElement = function (nums, val) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  return i;
+};
