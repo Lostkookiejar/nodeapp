113ea4f540fd3c4a5a696529e959d34ddab4c6f7
Chong Rui Xin <rxchong1123@gmail.com>
2026-07-17 10:56:41 +0800

    Merge Two Sorted Lists

--- code1.js
+++ code1.js
@@ -163,3 +163,23 @@ var removeElement = function (nums, val) {
   }
   return i;
 };
+
+var mergeTwoLists = function (list1, list2) {
+  let dummy = new ListNode();
+  let cur = dummy;
+
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      cur.next = list2;
+      list2 = list2.next;
+    } else {
+      cur.next = list1;
+      list1 = list1.next;
+    }
+    cur = cur.next;
+  }
+
+  cur.next = list1 || list2;
+
+  return dummy.next;
+};
