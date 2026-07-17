2e6a12450385237d51e7cd42a39b2c09a551838b
Chong Rui Xin <rxchong1123@gmail.com>
2026-07-17 10:57:21 +0800

    Longest Common Prefix

--- code1.js
+++ code1.js
@@ -183,3 +183,21 @@ var mergeTwoLists = function (list1, list2) {
 
   return dummy.next;
 };
+
+var longestCommonPrefix = function (strs) {
+  let pref = strs[0];
+  let prefLen = pref.length;
+
+  for (let i = 1; i < strs.length; i++) {
+    let s = strs[i];
+    while (pref !== s.substring(0, prefLen)) {
+      prefLen--;
+      if (prefLen === 0) {
+        return "";
+      }
+      pref = pref.substring(0, prefLen);
+    }
+  }
+
+  return pref;
+};
