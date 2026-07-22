299c6fcccc823553bc0e0bd93626673cce7585f7
Chong Rui Xin <rxchong1123@gmail.com>
2026-07-22 15:01:57 +0800

    combination sum

--- code1.js
+++ code1.js
@@ -226,3 +226,26 @@ const shiftGrid = (grid, k) => {
   shift(k, n - 1);
   return grid;
 };
+
+var combinationSum = function (candidates, target) {
+  const res = [];
+
+  function makeCombination(idx, comb, total) {
+    if (total === target) {
+      res.push([...comb]);
+      return;
+    }
+
+    if (total > target || idx >= candidates.length) {
+      return;
+    }
+
+    comb.push(candidates[idx]);
+    makeCombination(idx, comb, total + candidates[idx]);
+    comb.pop();
+    makeCombination(idx + 1, comb, total);
+  }
+
+  makeCombination(0, [], 0);
+  return res;
+};
