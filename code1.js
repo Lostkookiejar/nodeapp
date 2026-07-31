86641341b439c72a66bcd7e2a060cc2f2eaef5a3
Chong Rui Xin <rxchong1123@gmail.com>
2026-07-31 12:50:40 +0800

    minimum number of pushes to type word 2

--- code1.js
+++ code1.js
@@ -340,3 +340,19 @@ var minimumPushes = function (word) {
   }
   return result;
 };
+
+var minimumPushes = function (word) {
+  let freq = new Array(26).fill(0);
+
+  for (let i = 0; i < word.length; i++) {
+    freq[word.charCodeAt(i) - 97]++;
+  }
+
+  freq.sort((a, b) => b - a);
+
+  let ans = 0;
+  for (let i = 0; i < 26; i++) {
+    ans += (Math.floor(i / 8) + 1) * freq[i];
+  }
+  return ans;
+};
