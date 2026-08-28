4c5784a7fb02d17be6bc1ba985189f991e5cb8be
Chong Rui Xin <rxchong1123@gmail.com>
2026-08-28 16:06:10 +0800

    7 kyu collision detection

--- code1.js
+++ code1.js
@@ -650,3 +650,10 @@ function firstNonRepeated(s) {
   }
   return null; // your code here
 }
+
+function collision(x1, y1, radius1, x2, y2, radius2) {
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  const distance = Math.sqrt(dx * dx + dy * dy);
+  return distance <= radius1 + radius2; // collision?
+}
