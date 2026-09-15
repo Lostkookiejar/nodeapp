6e999eea4efb1a2c1bc3103d709ecbb3fb5d2b6a
Chong Rui Xin <rxchong1123@gmail.com>
2026-09-15 08:07:23 +0800

    8 kyu keep up the hoop

--- code1.js
+++ code1.js
@@ -746,3 +746,9 @@ function transposeTwoStrings(input) {
   return lines.join("\n");
   return "";
 }
+
+function hoopCount(n) {
+  return n < 10
+    ? "Keep at it until you get it"
+    : "Great, now move on to tricks";
+}
